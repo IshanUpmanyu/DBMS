69d464de1eede8980ef6f3736e68beeaa48dbf9e
ishan.upamanyu <ishan.upamanyu@fivetran.com>
2023-05-20 08:13:27 +0530

    up

--- class_queries.sql
+++ class_queries.sql
@@ -12,3 +12,26 @@ create table students (
    psp float
 );
 
+
+create table batches (
+	id INT PRIMARY KEY,
+    name varchar(20)
+);
+
+create table student_batches (
+	student_id INT,
+    batch_id INT,
+    start_date date,
+    
+    PRIMARY KEY(batch_id, student_id),
+    CONSTRAINT fk_student_batches_students foreign key (student_id) references students(id),
+    CONSTRAINT fk_student_batches_batches foreign key (batch_id) references batches(id)
+    
+);
+
+-- We created foreingn key, so that in student_batches table we cannot enter student_id or batch_ids that 
+-- are not present in the students or batches tables.
+
+
+-- 5 min break, we will start again at 8:15 AM
+
