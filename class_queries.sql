676257eb782af42856a745327480ae042ecb12f4
ishan.upamanyu <ishan.upamanyu@fivetran.com>
2023-05-20 07:57:14 +0530

    up

--- class_queries.sql
+++ class_queries.sql
@@ -1,1 +1,14 @@
 -- the queries we do in class, will be available here
+
+use scaler_class;
+
+create table students (
+   id INT PRIMARY KEY,
+   name varchar(20),
+   grad_year year,
+   university varchar(30),
+   email varchar(20),
+   phone_number varchar(20),
+   psp float
+);
+
