6fcc32c96ebfd5a4bcbf65152e0f5a9db7e2fd62
ishan.upamanyu <ishan.upamanyu@fivetran.com>
2023-05-24 07:28:36 +0530

    clear

--- class_queries.sql
+++ class_queries.sql
@@ -1,2 +1,6 @@
 -- the queries we do in class, will be available here
 
+select distinct last_name from sql_store.customers;
+
+-- Distinct works on the combination of columns. The whole combination must be unique.
+select distinct first_name, last_name from sql_store.customers;
