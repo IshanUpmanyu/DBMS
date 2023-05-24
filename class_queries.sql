fe4983d277407fab510815bf1dd3981576f92969
ishan.upamanyu <ishan.upamanyu@fivetran.com>
2023-05-24 08:04:51 +0530

    up

--- class_queries.sql
+++ class_queries.sql
@@ -4,3 +4,35 @@ select distinct last_name from sql_store.customers;
 
 -- Distinct works on the combination of columns. The whole combination must be unique.
 select distinct first_name, last_name from sql_store.customers;
+
+
+-- Comparison operations
+
+select * from sql_store.customers;
+
+select * from sql_store.customers
+where points  > 500;
+
+select * from sql_store.customers
+where customer_id != 10;
+
+-- or do
+
+select * from sql_store.customers
+where customer_id <> 10;
+
+-- Get customers whose square of points is > 500
+select * from sql_store.customers
+where points * points > 500;
+-- NOte that you can do operations in where clause.
+
+
+-- Multiple Conditions
+select * from sql_store.customers
+where customer_id > 10
+and points > 100;
+
+-- Break till 8:10 AM.
+
+
+
