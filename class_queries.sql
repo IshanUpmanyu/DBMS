e7b220a4a08fff905abf243e82746212c3204a51
ishan.upamanyu <ishan.upamanyu@fivetran.com>
2023-06-04 06:39:59 +0530

    clear

--- class_queries.sql
+++ class_queries.sql
@@ -1,26 +1,2 @@
-
-
-create fulltext index idx_posts_title_body
-on sql_blog.posts(title, body);
-
-
--- Either react or redux
-select * 
-from sql_blog.posts
-where match(title, body) 
-against ("react redux" in boolean mode);
-
--- Must have react, redux is optional
-
-select * 
-from sql_blog.posts
-where match(title, body) 
-against ("+react redux" in boolean mode);
-
-
--- must have react, may have redux, must not have the word form
-select * 
-from sql_blog.posts
-where match(title, body) 
-against ("+react redux -form" in boolean mode);
+-- Queries go here
 
